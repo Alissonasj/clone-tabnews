9fede9d45804fafd9e9d59445f71b6b0964f7e17
Alisson Silva <30477408+Alissonasj@users.noreply.github.com>
2024-11-14 01:45:28 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
-export default function Home(){
-    return <h1>Não se alegre apenas com o resultado, mas também com processo em que te levou a este resultado.</h1>
+export default function Home() {
+  return (
+    <h1>
+      Não se alegre apenas com o resultado, mas também com processo em que te
+      levou a este resultado.
+    </h1>
+  );
 }
