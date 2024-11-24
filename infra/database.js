81b1dd4756d6d6c9c9588e1a6b91dd30a659cf95
Alisson Silva <30477408+Alissonasj@users.noreply.github.com>
2024-11-24 14:52:46 +0000

    add `database.js` and `.env` files

---
+++ infra/database.js
@@ -0,0 +1,20 @@
+import { Client } from "pg";
+
+async function query(queryObject) {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+  });
+
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
+}
+
+export default {
+  query: query,
+};
