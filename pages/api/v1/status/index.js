9f4071696d9d4f6d0f11d3427ba6646ab823c476
Alisson Silva <30477408+Alissonasj@users.noreply.github.com>
2024-12-01 14:14:33 +0000

    creating json object from server status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,15 @@
 import database from "infra/database.js";
 
 export default async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
+  const resultDbStatus = await database.query(`SELECT json_build_object(
+    'version', version(),
+    'max_connections', current_setting('max_connections')::INTEGER,
+    'current_connections', (SELECT COUNT(*) FROM pg_stat_activity)
+    ) AS db_status`);
 
-  response.status(200).json({ chave: "valor" });
+  const dbStatus = resultDbStatus.rows[0].db_status;
+
+  const updatedAt = new Date().toISOString();
+
+  response.status(200).json({ updated_at: updatedAt, db_status: dbStatus });
 }
