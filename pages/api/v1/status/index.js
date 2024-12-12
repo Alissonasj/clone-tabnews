eec31a378b87cf8eee4d953177a42f4b0b3e2401
alissonasj <alissonsilva.jesus@gmail.com>
2024-12-11 22:52:56 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,39 +1,25 @@
 import database from "infra/database.js";
 
-/*
-  Objeto Status Server
-  
-  updated_at: '',
-  dependencies: {
-    database: {
-      status: '',
-      max_connection: '',
-      opned_connections: '',
-      latency: '',
-      version: ''
-    },
-    webserver: ''
-  }
-*/
-
 export default async function status(request, response) {
-  // const resultDbStatus = await database.query(`SELECT json_build_object(
-  //   'version', version(),
-  //   'max_connections', current_setting('max_connections')::INTEGER,
-  //   'current_connections', (SELECT COUNT(*) FROM pg_stat_activity)
-  //   ) AS db_status`);
+  const databaseVersionResult = await database.query("SHOW server_version;");
 
-  const dbVersionResult = await database.query("SHOW server_version;");
-  const dbVersionValue = dbVersionResult.rows[0].server_version;
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const dbMaxConnectionResult = await database.query("SHOW max_connections;");
-  const dbMaxConnectionValue = dbMaxConnectionResult.rows[0].max_connections;
-
-  const dbCurrentConnectionsResult = await database.query(
-    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
   );
-  const dbCurrentConnectionsValue = dbCurrentConnectionsResult.rows[0].count;
-  console.log(dbCurrentConnectionsValue);
+
+  const databaseMaxConnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseCurrentConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+
+  const databaseCurrentConnectionsValue =
+    databaseCurrentConnectionsResult.rows[0].count;
 
   const updatedAt = new Date().toISOString();
 
@@ -42,10 +28,10 @@ export default async function status(request, response) {
     dependencies: {
       database: {
         status: "",
-        max_connections: parseInt(dbMaxConnectionValue),
-        opned_connections: dbCurrentConnectionsValue,
+        max_connections: parseInt(databaseMaxConnectionValue),
+        opned_connections: databaseCurrentConnectionsValue,
         latency: "",
-        version: dbVersionValue,
+        version: databaseVersionValue,
       },
       webserver: "",
     },
