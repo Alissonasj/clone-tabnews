3f03a65d49589adc1ce118886256627bb7dbc941
alissonasj <alissonsilva.jesus@gmail.com>
2024-12-12 15:15:40 -0300

    move `await client.conect()` inside to `try` and add `throw error`

--- infra/database.js
+++ infra/database.js
@@ -9,11 +9,20 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
   });
 
+  console.log("Configurações de ambiente Postgres: ", {
+    host: process.env.POSTGRES_HOST,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+  });
+
   try {
     await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
+    console.error(error);
     throw error;
   } finally {
     await client.end();
